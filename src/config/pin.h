baac5357df4cdf27735f7afb777dee8d3f06d8df
Leonardo <leocode1079@gmail.com>
2026-07-21 20:41:15 +0800

    feat: add pins configuration

---
+++ src/config/pin.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace pin {
+    const unsigned int THERMO_CLK_PIN = 14;
+    const unsigned int THERMO_CS_PIN = 27;
+    const unsigned int THERMO_SO_PIN = 13;
+    const unsigned int SDA_PIN = 21;
+    const unsigned int SCL_PIN = 22;
+    const unsigned int SERVO_VALVE_PIN = 25;
+    const unsigned int ROTARY_ENCODER_CLK = 32;
+    const unsigned int ROTARY_ENCODER_DT = 33;
+    const unsigned int ROTARY_ENCODER_SW = 26;
+    const unsigned int PUMP_PIN = 16;
+    const unsigned int ELECTRICAL_LIGHTER_PIN = 17;
+    const unsigned int IR_FLAME_OUTPUT_PIN = 34;
+    const unsigned int MAX_FLOAT_SWITCH_PIN = 39;
+    const unsigned int MIN_FLOAT_SWITCH_PIN = 35;
+}
