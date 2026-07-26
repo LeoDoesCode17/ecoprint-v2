dcc83ba1a3bac52dcbcb4b68dcf4cd41de3211fb
Leonardo <leocode1079@gmail.com>
2026-07-26 14:04:47 +0800

    feat: modify constants for servo valve percent degree mapping

--- src/config/constants.h
+++ src/config/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace constant {
-    const unsigned int ZERO_PERCENT_DEGREE = 12;
-    const unsigned int VALVE_PERCENT_TO_DEGREE_GRADIENT = 78;
-    const unsigned int VALVE_PERCENT_TO_DEGREE_OFFSET = 12;
+    const unsigned int ZERO_PERCENT_DEGREE = 6;
+    const unsigned int VALVE_PERCENT_TO_DEGREE_GRADIENT = 94;
+    const unsigned int VALVE_PERCENT_TO_DEGREE_OFFSET = 6;
 }
