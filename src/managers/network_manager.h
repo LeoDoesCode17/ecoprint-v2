7ae86a6cd09dca210fb78294f737059d66c8f04b
Leonardo <leocode1079@gmail.com>
2026-08-02 23:02:08 +0800

    feat: add mqtt loop function

--- src/managers/network_manager.h
+++ src/managers/network_manager.h
@@ -5,4 +5,5 @@ namespace network_manager {
     void conect_or_reconnect();
     void publish_device_status(ecoprint_device_t device_status);
     void publish_sensor_data(ecoprint_sensor_t sensor_data);
+    void mqtt_loop();
 }
