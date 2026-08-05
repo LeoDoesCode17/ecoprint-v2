cb45b0188ce5dc8271dde4db85caded8e37def37
Leonardo <leocode1079@gmail.com>
2026-08-05 12:40:17 +0800

    feat: add event varible into ecoprint sensor type

--- src/config/type.h
+++ src/config/type.h
@@ -1,18 +1,28 @@
 #pragma once
 #include <stdint.h>
 using mqtt_callback = void (*)(char *, uint8_t *, unsigned int);
+
+enum class EcoprintEvent : uint8_t
+{
+    PREPARATION = 0,
+    STEAMING = 1,
+};
+
 typedef struct
 {
     float water_temperature;
     float air_temperature;
     float air_humidity;
     bool is_water_sufficient;
+    EcoprintEvent event;
 } ecoprint_sensor_t;
+
 typedef struct
 {
     int state_machine;
     bool is_active;
 } ecoprint_device_t;
+
 enum state_machine
 {
     IDLE,
