4d88db2b577f13a32cd159c41af844ad1128f3a1
Leonardo <leocode1079@gmail.com>
2026-08-05 19:00:14 +0800

    feat: add tft module

---
+++ src/displays/tft_ili9488.cpp
@@ -0,0 +1,9 @@
+#include "tft_ili9488.h"
+
+namespace tft_ili9488
+{
+    void initialize()
+    {
+        
+    }
+} // namespace tft_ili9488
---
+++ src/displays/tft_ili9488.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace tft_ili9488
+{
+    void initialize();
+} // namespace tft_ili9488
