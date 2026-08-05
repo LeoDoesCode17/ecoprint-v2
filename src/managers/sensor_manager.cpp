9912e7487dcfbbabcbf6bef19d6eb9034626110e
Leonardo <leocode1079@gmail.com>
2026-08-05 19:00:46 +0800

    feat: add rotary encoder in sensor manager

--- src/managers/sensor_manager.cpp
+++ src/managers/sensor_manager.cpp
@@ -1,12 +1,15 @@
 #include "sensor_manager.h"
 #include "sensors/thermocouple.h"
 #include "sensors/sht3x.h"
+#include "sensors/rotary_encoder.h"
+#include "config/pin.h"
 
 namespace sensor_manager
 {
     void initialize()
     {
         sht3x::initialize();
+        rotary_encoder::initialize(pin::ROTARY_ENCODER_DT, pin::ROTARY_ENCODER_CLK, pin::ROTARY_ENCODER_SW);
     }
     float sht3x_temperature_celcius()
     {
@@ -19,5 +22,19 @@ namespace sensor_manager
     float thermocouple_temperature_celcius()
     {
         return thermocouple::temperature_celcius();
+    }
+    long getEncoderPosition()
+    {
+        return rotary_encoder::getPosition();
+    }
+
+    void setEncoderPosition(long position)
+    {
+        rotary_encoder::setPosition(position);
+    }
+
+    bool wasEncoderButtonPressed()
+    {
+        return rotary_encoder::wasButtonPressed();
     }
 }
--- src/managers/sensor_manager.h
+++ src/managers/sensor_manager.h
@@ -1,8 +1,12 @@
 #pragma once
 
-namespace sensor_manager {
+namespace sensor_manager
+{
     void initialize();
     float thermocouple_temperature_celcius();
     float sht3x_temperature_celcius();
     float sht3x_humidity_percent();
+    long getEncoderPosition();
+    void setEncoderPosition(long position);
+    bool wasEncoderButtonPressed();
 }
