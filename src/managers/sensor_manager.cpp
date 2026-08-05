7c14ee9d6ac1150f7bf55eed9927c6d6a595f7a8
Leonardo <leocode1079@gmail.com>
2026-08-05 12:44:36 +0800

    feat: add sht3x implmentation in sensor network

--- src/managers/sensor_manager.cpp
+++ src/managers/sensor_manager.cpp
@@ -1,8 +1,21 @@
 #include "sensor_manager.h"
 #include "sensors/thermocouple.h"
+#include "sensors/sht3x.h"
 
 namespace sensor_manager
 {
+    void initialize()
+    {
+        sht3x::initialize();
+    }
+    float sht3x_temperature_celcius()
+    {
+        return sht3x::temprature_celcius();
+    }
+    float sht3x_humidity_percent()
+    {
+        return sht3x::humidity_percent();
+    }
     float thermocouple_temperature_celcius()
     {
         return thermocouple::temperature_celcius();
--- src/managers/sensor_manager.h
+++ src/managers/sensor_manager.h
@@ -1,5 +1,8 @@
 #pragma once
 
 namespace sensor_manager {
+    void initialize();
     float thermocouple_temperature_celcius();
+    float sht3x_temperature_celcius();
+    float sht3x_humidity_percent();
 }
