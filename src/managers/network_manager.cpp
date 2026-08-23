aba64b0baf81cf7f5bdf35d3095bc70223e09028
leonardo <leocode1079@gmail.com>
2026-08-23 20:04:26 +0800

    feat: add field is valve open and setpoint when publish sensor data

--- src/managers/network_manager.cpp
+++ src/managers/network_manager.cpp
@@ -186,6 +186,8 @@ namespace network_manager
         const float air_temperature = sensor_data.air_temperature;
         const float air_humidity = sensor_data.air_humidity;
         const bool is_water_sufficient = sensor_data.is_water_sufficient;
+        const bool is_valve_open = sensor_data.is_valve_open;
+        const float setpoint_temperature = sensor_data.setpoint_temperature;
         char recorded_at[ISO8601_BUFFER_SIZE];
         if (!get_iso8601_utc(recorded_at, sizeof(recorded_at)))
         {
@@ -199,6 +201,8 @@ namespace network_manager
         doc["humidity"] = air_humidity;
         doc["water_sufficient"] = is_water_sufficient;
         doc["recorded_at"] = recorded_at;
+        doc["setpoint"] = setpoint_temperature;
+        doc["is_valve_open"] = is_valve_open;
 
         char payload[SENSOR_MESSAGE_BUFFER_SIZE];
         serializeJson(doc, payload);
