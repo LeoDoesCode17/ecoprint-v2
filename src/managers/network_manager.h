fcfa1d9a5fdbd0a9c1853d3acc0db9c1a4a4bcb7
leonardo <leocode1079@gmail.com>
2026-09-10 07:01:36 +0800

    feat: add publish actuator data function definition

--- src/managers/network_manager.h
+++ src/managers/network_manager.h
@@ -6,6 +6,7 @@ namespace network_manager {
     void conect_or_reconnect();
     void publish_device_status(ecoprint_device_t device_status);
     void publish_sensor_data(ecoprint_sensor_t sensor_data);
+    void publish_actuator_data(ecoprint_actuator_t &actuator_data);
     void mqtt_loop();
     void set_mqtt_callback(mqtt_callback callback);
     void publish_ema_filtered_sensor_data(ema_filter_sensor_data_t sensor_data);
