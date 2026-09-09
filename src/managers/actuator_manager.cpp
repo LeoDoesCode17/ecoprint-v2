f168f608fdb9d5fe5266920feebb0ffefc068739
leonardo <leocode1079@gmail.com>
2026-09-10 07:24:30 +0800

    feat: add functions to open wide and narrow valve, get current valve degree and is valve max opening

--- src/managers/actuator_manager.cpp
+++ src/managers/actuator_manager.cpp
@@ -9,6 +9,8 @@ namespace
     static bool _is_valve_open = false;
     static bool _is_pump_on = false;
     static bool _is_lighter_on = false;
+    static int _current_valve_degree = 0;
+    static bool _is_valve_max_opening = false;
 } // namespace
 
 namespace actuator_manager
@@ -20,23 +22,57 @@ namespace actuator_manager
     }
     void open_valve_by_percent(float opening_percentage)
     {
+        if (!_is_valve_open)
+            _is_valve_open = true;
         unsigned int degree = (int)(constant::VALVE_PERCENT_TO_DEGREE_GRADIENT * opening_percentage + constant::VALVE_PERCENT_TO_DEGREE_OFFSET);
         servo_valve::open_by_degree(degree);
+        // _is_valve_max_opening = ?
     }
     void close_valve()
     {
+        if (_is_valve_open)
+            _is_valve_open = false;
         servo_valve::close();
-        _is_valve_open = false;
+        _current_valve_degree = 0;
+        _is_valve_max_opening = false;
     }
     void open_valve_by_degree(int degree)
     {
+        if (!_is_valve_open)
+            _is_valve_open = true;
         servo_valve::open_by_degree(degree);
-        _is_valve_open = true;
+        _current_valve_degree = degree;
+        // _is_valve_max_opening = ?
+    }
+    void open_wide_valve()
+    {
+        if (!_is_valve_open)
+            _is_valve_open = true;
+        servo_valve::open_by_degree(30);
+        _current_valve_degree = 30;
+        _is_valve_max_opening = true;
+    }
+    void open_narrow_valve()
+    {
+        if (!_is_valve_open)
+            _is_valve_open = true;
+        servo_valve::open_by_degree(9);
+        _current_valve_degree = 9;
+        _is_valve_max_opening = false;
     }
     bool is_valve_open()
     {
         return _is_valve_open;
     }
+    int get_current_valve_degree()
+    {
+        return _current_valve_degree;
+    }
+    bool is_valve_max_opening()
+    {
+        return _is_valve_max_opening;
+    }
+
     void turn_on_pump()
     {
         pump::on();
--- src/managers/actuator_manager.h
+++ src/managers/actuator_manager.h
@@ -6,7 +6,12 @@ namespace actuator_manager
     void open_valve_by_percent(float opening_percentage);
     void open_valve_by_degree(int degree);
     void close_valve();
+    void open_wide_valve();
+    void open_narrow_valve();
     bool is_valve_open();
+    int get_current_valve_degree();
+    bool is_valve_max_opening();
+
     
     void turn_on_pump();
     void turn_off_pump();
