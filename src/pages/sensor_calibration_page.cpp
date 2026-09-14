58a6b9b6987fdbf2e4c871da4b9fb2c32d6e254b
Leonardo <leocode1079@gmail.com>
2026-09-14 13:03:51 +0800

    feat: make buttons to navigate into thermocouple calibration in sensor calibration page

--- src/pages/sensor_calibration_page.cpp
+++ src/pages/sensor_calibration_page.cpp
@@ -5,6 +5,30 @@
 namespace pages
 {
     SensorCalibrationPage sensorCalibrationPage;
+
+    namespace
+    {
+        struct SensorItem
+        {
+            const char *label;
+            PageId target;
+        };
+
+        const SensorItem items[] = {
+            {"Thermocouple", PageId::ThermocoupleCalibration},
+            {"Back", PageId::Setting},
+        };
+
+        const int itemCount = sizeof(items) / sizeof(items[0]);
+
+        const int buttonHeight = 60;
+
+        const int buttonHeight = 60;
+        const int buttonGap = 15;
+        const int marginX = 20;
+        const int marginTop = 30;
+    }
+
     void SensorCalibrationPage::onEnter(TFT_eSPI &tft)
     {
         _selectedIndex = 0;
@@ -14,18 +38,48 @@ namespace pages
         tft.setTextColor(TFT_WHITE, TFT_BLACK);
         tft.setTextFont(4);
         tft.drawString("SENSOR CALIBRATION", tft.width() / 2, 60);
+
+        for (size_t i = 0; i < itemCount; i++)
+        {
+            drawButton(tft, i);
+        }
     }
     void SensorCalibrationPage::onExit()
     {
     }
     void SensorCalibrationPage::update(TFT_eSPI &tft, long encoderDelta, bool buttonPressed)
     {
+        if (encoderDelta != 0)
+        {
+            int previous = _selectedIndex;
+
+            _selectedIndex = (_selectedIndex + static_cast<int>(encoderDelta)) % itemCount;
+            if (_selectedIndex < 0)
+            {
+                _selectedIndex += itemCount;
+            }
+
+            if (_selectedIndex != previous)
+            {
+                drawButton(tft, previous);
+                drawButton(tft, _selectedIndex);
+            }
+        }
         if (buttonPressed)
         {
-            page_manager::navigateTo(PageId::Setting);
+            page_manager::navigateTo(items[_selectedIndex].target);
         }
     }
     void SensorCalibrationPage::drawButton(TFT_eSPI &tft, int index)
     {
+        int y = marginTop + index * (buttonHeight + buttonGap);
+        bool selected = (index == _selectedIndex);
+        uint16_t fillColor = selected ? (items[_selectedIndex].target == PageId::Menu ? TFT_RED : TFT_BLUE) : TFT_DARKGREY;
+
+        tft.fillRoundRect(marginX, y, tft.width() - 2 * marginX, buttonHeight, 8, fillColor);
+        tft.setTextDatum(MC_DATUM);
+        tft.setTextColor(TFT_WHITE, fillColor);
+        tft.setTextFont(4);
+        tft.drawString(items[index].label, tft.width() / 2, y + buttonHeight / 2);
     }
 }
