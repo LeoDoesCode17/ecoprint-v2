c944a60b84e483ab54a3122a0cf0352744598caa
Leonardo <leocode1079@gmail.com>
2026-09-14 12:45:58 +0800

    feat: change page title

--- src/pages/setting_page.cpp
+++ src/pages/setting_page.cpp
@@ -36,7 +36,7 @@ namespace pages
         tft.setTextDatum(MC_DATUM);
         tft.setTextColor(TFT_WHITE, TFT_BLACK);
         tft.setTextFont(4);
-        tft.drawString("SENSOR CALIBRATION", tft.width() / 2, 60);
+        tft.drawString("SETTING", tft.width() / 2, 60);
         tft.fillScreen(TFT_BLACK);
         for (size_t i = 0; i < itemCount; i++)
         {
